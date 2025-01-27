2cd136f05a29d26e8d72d863c5133edbe0db35d3
adil elkhalloufi <adilelkhalloufi@gmail.com>
2025-01-27 15:39:00 +0100

    feat(footer) : new footer

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,7 +2,7 @@ import { CONFIG } from "@/CONFIG";
 import { AxiosError } from "axios";
 import { toast } from "sonner";
 
-export const API_URL = `http://127.0.0.1:8000/api`;
+export const API_URL = process.env.REACT_APP_API_URL;
 //export const API_URL = `https://oapi.adev.ma/api`;
 
 export enum NotificationType {
